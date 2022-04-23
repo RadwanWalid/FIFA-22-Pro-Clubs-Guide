4dc579cb9514c669067b96451557ab8bd0d65465
Radwan Walid <rado.king2001@gmail.com>
2022-04-23 08:09:58 +0200

    2

---
+++ Javascript/passing-js.js
@@ -0,0 +1,7 @@
+function onHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Passing/" + name + " On Hover.png";
+}
+
+function offHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Passing/" + name + ".png";
+}
---
+++ Javascript/shooting-js.js
@@ -0,0 +1,7 @@
+function onHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Defending/" + name + " On Hover.png";
+}
+
+function offHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Defending/" + name + ".png";
+}
