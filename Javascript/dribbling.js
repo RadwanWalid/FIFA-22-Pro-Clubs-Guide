722facbb54ec9e7a6c880a1ae4def032bd81b96e
Radwan Walid <rado.king2001@gmail.com>
2022-04-23 16:07:06 +0200

    3

---
+++ Javascript/dribbling.js
@@ -0,0 +1,7 @@
+function onHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Dribbling/" + name + " On Hover.png";
+}
+
+function offHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Dribbling/" + name + ".png";
+}
---
+++ Javascript/goalkeeping.js
@@ -0,0 +1,7 @@
+function onHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Goalkeeping/" + name + " On Hover.png";
+}
+
+function offHover(name, num) {
+  document.getElementById(name + "" + num).src = "Images/Skill Tree/Goalkeeping/" + name + ".png";
+}
