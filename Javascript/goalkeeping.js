e08714bfdd91ee7c068a1856f84e2e0b28bffb74
Radwan Walid <rado.king2001@gmail.com>
2022-05-02 02:49:35 +0200

    37

--- Javascript/goalkeeping.js
+++ Javascript/goalkeeping.js
@@ -9,6 +9,17 @@
 //   }
 //     document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + ".png";
 // }
+function showDescription(name, num) {
+  document.getElementById(name + "" + num + "-des").style.display = "initial";
+  document.getElementById(name + "-inc").style.width = "2px";
+  document.getElementById(name + "-growth-value").innerHTML = "+2";
+}
+
+function hideDescription(name, num) {
+  document.getElementById(name + "" + num + "-des").style.display = "none";
+  document.getElementById(name + "-inc").style.width = "0px";
+  document.getElementById(name + "-growth-value").innerHTML = "";
+}
 
 function select(name, num) {
   document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + " Selected.png";
