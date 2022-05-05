6bb0cc6b5fde4b1ffa9398e75368691ff6ce26ac
Radwan Walid <rado.king2001@gmail.com>
2022-05-06 01:36:02 +0200

    39(hardcode continued)

--- Javascript/goalkeeping.js
+++ Javascript/goalkeeping.js
@@ -15,7 +15,6 @@ var path3 = [reflexes1, positioning1, reflexes2, wall1];
 var ref1branch = document.getElementsByClassName("from-ref1-to-branch")[0];
 var ref1kic1 = document.getElementsByClassName("from-ref1-to-kic1")[0];
 var kic1kic2 = document.getElementsByClassName("from-kic1-to-kic2")[0];
-
 var line1 = [ref1branch, ref1kic1, kic1kic2];
 
 
@@ -35,24 +34,36 @@ function select(name, num) {
   var select = document.getElementById(name + "" + num);
   var index1 = parseInt(path1.indexOf(select));
   if(path1[index1].id == "Reflexes1") {
-    document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + " Selected.png";
-    document.getElementById(name + "" + num).onclick = function() {unselect(name, num);}
+    select.src = "../Images/Skill Tree/Goalkeeping/" + name + " Selected.png";
+    select.onclick = function() {unselect(path1[index1].id.substring(0,path1[index1].id.length-1), path1[index1].id.substring(path1[index1].id.length-1,path1[index1].id.length));}
   } else {
-    if(path1[index1-1].src == "../Images/Skill Tree/Goalkeeping/" + name + " Selected.png") {
-      document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + " Selected.png";
-      document.getElementById(name + "" + num).onclick = function() {unselect(name, num);}
-      line1[index1-1].style.borderTop = "20% solid #0A8BFF";
-    } else {
       for(var i = 0; i <= index1; i++) {
-        path1[i].src = "../Images/Skill Tree/Goalkeeping/" + path1[i].id.substring(0,path1[i].id.length-1) + " Selected.png";
-        path1[i.onclick] = function() {unselect(path1[i].id.substring(0,path1[i].id.length-1), path1[i].id.substring(path1[i].id.length-1,path1[i].id.length));}
+        let newname = path1[i].id.substring(0,path1[i].id.length-1);
+        let newnum = path1[i].id.substring(path1[i].id.length-1,path1[i].id.length);
+        path1[i].src = "../Images/Skill Tree/Goalkeeping/" + newname + " Selected.png";
+        path1[i].onclick = function() { unselect(newname, newnum); }
         line1[i].style.borderTop = "35px solid #0A8BFF";
       }
-    }
   }
 }
 
 function unselect(name, num) {
-  document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + ".png";
-  document.getElementById(name + "" + num).onclick = function() {select(name, num);}
+  var unselect = document.getElementById(name + "" + num);
+  var index2 = parseInt(path1.indexOf(unselect));
+  alert(unselect.id);
+  if(path1[index2].id == "Kicking2") {
+    document.getElementById(name + "" + num).src = "../Images/Skill Tree/Goalkeeping/" + name + ".png";
+    document.getElementById(name + "" + num).onclick = function() {select(name, num);}
+    line1[index2].style.borderTop = "0px";
+  } else {
+      for(var j =  path1.length-1; j >= index2; j--) {
+        let newnameun = path1[j].id.substring(0 ,path1[j].id.length-1);
+        let newnumun = path1[j].id.substring(path1[j].id.length-1, path1[j].id.length);
+        path1[j].src = "../Images/Skill Tree/Goalkeeping/" + newnameun + ".png";
+        path1[j].onclick = function() {select(newnameun, newnumun);}
+        if(j==index2 && index2!=0)
+          line1[j-1].style.borderTop = "0px";
+        line1[j].style.borderTop = "0px";
+      }
+    }
 }
